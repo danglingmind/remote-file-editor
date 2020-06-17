029e83870cf5ce38f97bde2bbb01ff6862090fb9
ricky27 <prateekreddy274@gmail.com>
2020-06-17 19:03:23 +0530

    conversion into class model

--- src/remote_client.py
+++ src/remote_client.py
@@ -2,14 +2,224 @@
 """ remote side client process """
 import configparser
 import os
-import platform
 import socket
 import subprocess as sp
 import sys
 import threading
 import time
 import logging
+from platform import system
 
+
+class NewHostConnection:
+    def __init__(self, ip, port):
+        self.host_ip = ip
+        self.port = port
+        self.__host_sock = None
+        self.__connection_time = time.time()
+        self.__refresh_flag = False
+
+    @property
+    def host_sock(self):
+        return self.__host_sock
+
+    @property
+    def connection_time(self):
+        return self.__connection_time
+
+    @property
+    def refresh_flag(self):
+        return self.__refresh_flag
+
+    @refresh_flag.setter
+    def refresh_flag(self, value):
+        self.__refresh_flag = value
+
+    @connection_time.setter
+    def connection_time(self, value):
+        self.__connection_time = value
+
+    @host_sock.setter
+    def host_sock(self, value):
+        self.__host_sock = value
+
+    def connect_to_host(self):
+        try:
+            new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            new_sock.connect((self.__host_sock, self.port))
+            self.__connection_time = time.time()
+            self.__host_sock = new_sock
+        except socket.error as e:
+            print('Error: '+str(e))
+
+    def reconnect(self):
+        try:
+            # create a new connection before closing the existing one
+            new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            new_sock.connect((self.__host_sock, self.port))
+            # close existing socket
+            self.__host_sock.close()
+            # set the new one
+            self.__host_sock = new_sock
+            # change connection_time
+            self.__connection_time = time.time()
+        except socket.error as e:
+            print('Error: '+str(e))
+
+    def _refresh_daemon(self, interval: int):
+        while self.__refresh_flag:
+            # after interval time reconnect, which will basically refresh the socket
+            time.sleep(3)
+            if time.time() - self.__connection_time > interval:
+                self.reconnect()
+
+    def refresh(self, interval: int):
+        self.__refresh_flag = True
+        # initiate a daemon which will refresh the connection after the interval which is provided
+        th = threading.Thread(target=self._refresh_daemon(), daemon=True, args=[interval])
+        th.start()
+
+    def stop_refresh(self):
+        self.__refresh_flag = False
+
+    def close(self):
+        self.__host_sock.close()
+
+
+class Config:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.editor_mac = ''
+        self.editor_windows = ''
+        self.host_address = ''
+        self.platform = system()
+        self.config = configparser.ConfigParser()
+
+        # if file exists then read all the values
+        if os.path.exists(self.filepath):
+            self.config.read(self.filepath)
+
+            address = self.config['host']['address']
+            if len(address) == 0:
+                hIp = input('Enter host IP : ')
+                self.config['host']['address'] = hIp.strip()
+
+            editor = self.config['editor'][self.platform]
+            if len(editor) == 0:
+                ed = input('Enter editor path : ').strip()
+                self.config['editor'][self.platform] = ed
+
+        else:
+            # if file is not present
+            # create the file
+            self.config.add_section('host')
+            address = input('Enter the host address :').strip()
+            self.config['host']['address'] = address
+            self.host_address = address
+
+            self.config.add_section('editor')
+            if self.platform == 'Darwin':
+                ed = input('Enter Exact Same Name of the editor as shown in Applications : ').strip()
+                self.editor_mac = ed
+            elif self.platform == 'Windows':
+                ed = input('Enter editor\'s Executable path : ').strip()
+                self.editor_windows = ed
+
+            self.config['editor'][self.platform] = ed
+
+        # write/update the config file with values
+        with open(self.filepath, 'w') as configfile:
+            self.config.write(configfile)
+
+    def update_config(self):
+        print("Host Address : "+str(self.host_address))
+        if self.platform == 'Windows':
+            print("Editor : "+str(self.editor_windows))
+        else:
+            print("Editor : "+str(self.editor_mac))
+
+        want = input('Want to modify ? Y/N :\n').strip()
+        if want.lower() == 'y':
+            ch = input('Change host address Y/N').strip()
+            if ch.lower() == 'y':
+                self.host_address = input('New Address : ').strip()
+                self.config['host']['address'] = self.host_address
+            ch = input('Change Editor Y/N').strip()
+            if ch.lower() == 'y':
+                if self.platform == 'Windows':
+                    self.editor_windows = input('New Editor : ').strip()
+                    self.config['editor'][self.platform] = self.editor_windows
+                else:
+                    self.editor_mac = input('New Editor : ').strip()
+                    self.config['editor'][self.platform] = self.editor_mac
+
+        # write into the confi file
+        with open(self.filepath, 'w') as configfile:
+            self.config.write(configfile)
+
+
+class DirWatcher:
+    def __init__(self, path: str):
+        self.__dir_path = path
+        self.__watch = False
+
+    @property
+    def watch(self):
+        return self.__watch
+
+    @watch.setter
+    def watch(self, value):
+        self.__watch = value
+
+    def __send_file(self, filename):
+        # create a connection to host
+
+        # send the file
+        pass    #
+
+    def __watch_daemon(self):
+        while self.__watch:
+            time.sleep(1)
+            # check for any files to get change inside the directory provided
+            # file list
+            files = [f for _, _, f in os.walk(self.__dir_path)][0]
+            for fl in files:
+                pass
+            #TODO: implement files last modified record meta data
+
+    def start_watch(self):
+        self.__watch = True
+        th = threading.Thread(target=self.__watch_daemon, daemon=True, args=[])
+        th.start()
+
+    def stop_watch(self):
+        self.__watch = False
+
+
+class FileRecord:
+    def __init__(self):
+        self.db = {}
+
+    def add_file(self, filename: str, filepath: str, last_mod_time: time.time()):
+        self.db[filename] = (filepath, last_mod_time)
+
+    def get_file(self, filename):
+        if filename in self.db:
+            return self.db[filename]
+        else:
+            return None
+
+    def update_file(self, filename: str, mod_time: time.time()):
+        if filename in self.db:
+            self.db[filename][1] = mod_time
+        else:
+            return None
+
+
+#TODO : Dir Watcher class  : watcher will initiate a thread to send the file back to server
+#TODO : open file function
+#TODO : custom logging class
+#TODO : add authentication on startup (use init for authenticaion)(try to incorporate SSH key as auth)
 
 def configSetup():
     config = configparser.ConfigParser()
